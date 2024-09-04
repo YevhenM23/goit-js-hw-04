b1c15725f8aec6c78476982d9e571a93dc022c69
Yevhen Maslov <yevheniimaslov@gmail.com>
2024-09-04 22:35:03 +0300

    all done

---
+++ js/task-1.js
@@ -0,0 +1,28 @@
+function isEnoughCapacity(products, containerSize) {
+    let sum = 0;
+    const values = Object.values(products);
+    for (let index = 0; index < values.length; index++) {
+        sum += values[index];
+    }
+    if (sum < containerSize) {
+        return true;
+    }
+    return false;
+}
+
+
+
+
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+);
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+);
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+);
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+);
